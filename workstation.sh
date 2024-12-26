944ddd5e3c700da63b84d283197ac41caabc9bb9
Supraja <supra556699@gmail.com>
2024-12-26 23:09:44 +0800

    workstation

--- workstation.sh
+++ workstation.sh
@@ -17,18 +17,18 @@ sudo usermod -aG docker ec2-user
 sudo docker ps
 echo "success"
 
-curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.31.2/2024-11-15/bin/darwin/amd64/kubectl
-chmod +x ./kubectl
-sudo mv kubectl /usr/local/bin/kubectl
+# curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.31.2/2024-11-15/bin/darwin/amd64/kubectl
+# chmod +x ./kubectl
+# sudo mv kubectl /usr/local/bin/kubectl
 
 # #kubectl version
 # echo "success"
-# ARCH=amd64
-# PLATFORM=$(uname -s)_$ARCH
-# curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
-# tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
-# sudo mv /tmp/eksctl /usr/local/bin
-# eksctl version
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+sudo mv /tmp/eksctl /usr/local/bin
+eksctl version
 
-# echo "success"
-# aws configure
+echo "success"
+aws configure
