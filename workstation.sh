187450b2b2b5d176e5c5680d9c253cbc1860b545
Supraja <supra556699@gmail.com>
2024-12-26 22:55:26 +0800

    workstation

--- workstation.sh
+++ workstation.sh
@@ -12,7 +12,7 @@ sudo yum install -y yum-utils
 sudo yum config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin -y docker-compose-plugin -y
 sudo systemctl start docker
-sudo systemctl status docker
+# sudo systemctl status docker
 sudo usermod -aG docker ec2-user
 sudo docker ps
 echo "success"
